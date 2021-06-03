49d673739457388b0d9ca05f774bb06f451122d3
sparrow-jack0682 <sparrowjack0682@gmail.com>
2021-06-03 15:23:50 +0530

    my_sandbox_trial_101 with extra one line

--- sandbox.py
+++ sandbox.py
@@ -3,40 +3,39 @@
 # reference, or delete code that is no longer wanted.
 # 6. COMPARE ORIGINAL ARRAY AND ORDERED ARRAY AND BUILD A DICTIONARY TO REMEMBER ORIGINAL ORDERING (SPK).
 
-import numpy as np
-import re
+import  numpy as np
+cipher_acceptor = "g fytc y bmg "
+
+#create list of word that are their in the cipher_acceptor.
+cipher_lst = cipher_acceptor.split()
 
 
-def cipher_acceptor():
-    # The line below tells Python we're going to use the same
-    # global variable called cipher and that we are not creating
-    # a new function-specific one also called cipher
-    global cipher
-    # Get an input from the user
-    cipher = input("Enter the cipher:\n")
-    print(type(cipher))
-    # Make sure the user does not want to not quitting, and quit if a cipher is not entered
-    if not (cipher == 'X' or cipher == 'x' or cipher is None or cipher == ''):
-        # Check if input follows RegEx for a typical cryptoquip
-        if not re.search(r'^[a-zA-Z\s\D.,\-\'\"?!]+$', cipher):
-            # If it does not follow RegEx inform the user and restart the function
-            # and give the user an option to quit
-            print('\nInvalid cryptogram. Please retry or hit X to quit.\n\n')
-            cipher_acceptor()
-        else:
-            # If the RegEx is fine, replace double spaces with single spaces and remove trailing spaces
-            cipher = re.sub(r'(\s\s+)', " ", cipher.rstrip())  # rstrip() removes trailing spaces
-    else:
-        # Be courteous
-        quit("\nThank you for trying out this program.")
+# create an array from the above list.
+cipher_ary = np.array(cipher_lst)
 
-def main():
-    # Call acceptor() to accept an input cipher
-    cipher_acceptor()
-    # DELETE THE LINE BELOW WHEN THE PROGRAM IS COMPLETE
 
-csnvihicjlmvopakcs;kca
-djnfnjkncjknmc
+# create a length array from the cipher_array.
+cipher_len_lst = [] #create an empty list
+for i in cipher_lst:
+    word_len = len(i)
+    cipher_len_lst.append(word_len)
 
-if __name__ == '__main__':
-    main()
+
+#changing the cipher_len_lst in to an array.
+cipher_len_array = np.array(cipher_len_lst)
+
+
+# order the elements of cipher_len_ary.
+ord_cipher_len_ary = np.sort(cipher_len_array)
+
+
+#ordering cipher_ary by its length.
+ord_cipher_lst= sorted(cipher_lst,key=len)
+ord_cipher_ary = np.array(ord_cipher_lst)
+
+#we have the cipher array, ordered cipher array, length of the cipher array and the length of the ordered cipher array.
+print(cipher_ary)
+print(ord_cipher_ary)
+print(cipher_len_array)
+print(ord_cipher_len_ary)
+
