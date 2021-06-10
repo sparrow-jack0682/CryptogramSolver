a541d0e8ab961ea67da6b0f15ff3942e2c345994
sparrow-jack0682 <sparrowjack0682@gmail.com>
2021-06-10 10:34:20 +0530

    combination fuction... not sure how it helps.

--- trail_101.py
+++ trail_101.py
@@ -5,10 +5,9 @@ import enchant
 
 d = enchant.Dict("en_US")
 
-alphs = ['a b c d e f g h i j k l m n o p q r s t u v w x y z ']
-print(alphs,type(alphs))
 
-my_cipher = 'extreme'
+
+my_cipher = 'lloopsehwi'
 letter_lst = [x for x in my_cipher]
 print(letter_lst)
 
@@ -16,10 +15,11 @@ op = set()
 
 
 
-for n in range(4,len(my_cipher)):
+for n in range(2,len(my_cipher)):
+
     for y in list(combinations_with_replacement(letter_lst,n)):
         z = "".join(y)
         if d.check(z):
             op.add(z)
 print(op)
-
+print(len(op))
