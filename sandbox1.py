9e72e10ff5a9f4b6d6c8ba688db603743d5e3be6
sparrow-jack0682 <sparrowjack0682@gmail.com>
2021-06-17 11:49:22 +0530

    used json eng-dict... this works...

---
+++ sandbox1.py
@@ -0,0 +1,29 @@
+import json
+from difflib import get_close_matches
+
+#load JSON data
+data = json.load(open("data.json"))
+
+#take word from user
+word = input('Enter word: ')
+
+#function to return meaning of the word from data
+def getMeaning(w):
+    #for case sensitivity
+    w = w.lower()
+    #if-else to check word exist in our data or not
+    if w in data:
+        return data[w]
+    #give matching word
+    elif len(get_close_matches(w,data.keys())) > 0:
+        close_match = get_close_matches(w,data.keys())[0]
+
+#function call to get meaning of the word entered by user
+meaning = getMeaning(word)
+
+#printing meaning of the word in console
+if type(meaning) == list:
+    for item in meaning:
+        print(item)
+else:
+    print(meaning)
--- trail_101.py
+++ trail_101.py
@@ -2,24 +2,26 @@
 
 from itertools import combinations_with_replacement
 import enchant
+from PyDictionary import PyDictionary as pyd
 
 d = enchant.Dict("en_US")
 
 
 
-my_cipher = 'lloopsehwi'
+my_cipher = 'abcdefghij'
 letter_lst = [x for x in my_cipher]
 print(letter_lst)
 
-op = set()
+op = []
 
 
 
-for n in range(2,len(my_cipher)):
+for n in range(5,len(my_cipher)):
 
     for y in list(combinations_with_replacement(letter_lst,n)):
         z = "".join(y)
         if d.check(z):
-            op.add(z)
+            op.append(z)
+
 print(op)
-print(len(op))
+
