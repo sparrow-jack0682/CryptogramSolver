be1f6639eef3bb21f769414db4c35536c32fb709
sparrow-jack0682 <85030985+sparrow-jack0682@users.noreply.github.com>
2021-06-23 11:16:35 +0530

    Add files via upload

---
+++ trail_101.py
@@ -0,0 +1,36 @@
+# using regular expressions
+import re
+
+global l
+
+input_cipher = "hjg"
+l = len(input_cipher)
+
+my_text = """" a 
+aa
+aaa
+suhas
+b
+bb
+bbb
+xlgkx
+c
+cc
+ccc
+
+
+"""
+
+
+
+patterns = re.compile(r'(.){5,}')
+# matches = patterns.finditer(my_text)
+lines = my_text.split('\n')
+
+for line in lines:
+    rxx = patterns.search(line)
+    if rxx:
+        print(line)
+
+
+
